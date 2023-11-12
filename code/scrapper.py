5f2605a91287b277eda00ebf79c59b9727dfc7d8
haleyej <haleyej@umich.edu>
2023-11-12 14:04:02 -0500

    added caching for literally no reason

--- code/scrapper.py
+++ code/scrapper.py
@@ -1,33 +1,63 @@
 import os 
 from bs4 import BeautifulSoup
 import requests
-from requests import Response
+import json 
 
 
 class Scrapper():
-  def __init__(self, base: str, limit: int = None) -> None:
+  def __init__(self, base: str, limit: int = None, cache_file: str = None) -> None:
+    '''
+    Class to scrape search results from Google
+    '''
     self.base = base
     self.limit = limit
+
+    if cache_file != None:
+      if os.path.isfile(cache_file):
+        with open(cache_file) as f: 
+          cache = json.loads(f.read())
+      else:
+        # create file 
+        open(cache_file, 'a').close()
+        cache = {}
+      self.cache_file = cache_file
+      self.cache = cache
+      self.cached_queries = list(cache.keys())
+
 
   def prepare_query(self, query: str) -> str:
     return query.replace(' ', '+')
   
+
   def prepare_url(self, query: str) -> str:
     url = f"{self.base}q={query}"
     if self.limit != None: 
       url += f'&num{self.limit}'
     return url 
 
+
+  def add_to_cache(self, query: str, raw_text: str) -> None:
+    self.cache[query] = raw_text
+    with open(self.cache_file, 'w') as f: 
+      json.dump(self.cache, f)
+
+
   def query(self, query: str):
     query = self.prepare_query(query) 
-    url = self.prepare_url(query)
-    print(url)
-    raw = requests.get(url)
-    print(raw)
+    
+    if query in self.cache: 
+      raw_text = self.cache[query]
+    else: 
+      url = self.prepare_url(query)
+      raw_text = requests.get(url).text
+      self.add_to_cache(query, raw_text)
+    
+    processed_response = self.process_response(raw_text)
+    return processed_response
     
 
-  def process_response(self, raw: Response):
-    soup = BeautifulSoup(raw.text, "html.parser")
+  def process_response(self, raw_text: str):
+    soup = BeautifulSoup(raw_text, "html.parser")
     links = soup.find_all("a")
     headings = soup.find_all("h3")
 
@@ -40,14 +70,12 @@ class Scrapper():
           print(link.get('href').split("?q=")[1].split("&sa=U")[0])
           print("------")
       
-  def save_results(self, outfile: str):
-    pass
-      
+
 
 def main():
   query = "StackOverflow"
   base = 'https://google.com/search?'
-  scrapper = Scrapper(base, limit = 10)
+  scrapper = Scrapper(base, limit = 10, cache_file = 'files/cache.json')
   scrapper.query(query) 
 
 
