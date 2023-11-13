ebadc0807a43cf205a3ad7ae730a5ba1e14a0284
haleyej <haleyej@umich.edu>
2023-11-13 13:34:58 -0500

    cleaned up code

--- code/scrape_google_results.py
+++ code/scrape_google_results.py
@@ -17,12 +17,11 @@ def run_scrapper(base: str , queries: str, cache_file: str, save_path: str):
 
     for query in queries: 
         _ = scrapper.query(query, save_path = save_path)
-        print(_)
 
 def main():
     base = 'https://google.com/search?'
     queries = load_queries("eval/evaluation_queries.csv")
-    run_scrapper(base, queries[:5], cache_file = 'files/cache.json', save_path = 'files/processed_search_results/search.csv')
+    run_scrapper(base, queries, cache_file = 'files/cache.json', save_path = 'files/processed_search_results/search.csv')
 
 if __name__ == '__main__':
     main()
