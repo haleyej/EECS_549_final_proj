16754552f128236cedb7968fef3f2c20e36c88c7
haleyej <haleyej@umich.edu>
2023-11-14 22:17:45 -0500

    eval

--- code/scrape_google_results.py
+++ code/scrape_google_results.py
@@ -1,5 +1,6 @@
 from scrapper import Scrapper
 import csv 
+from tqdm import tqdm 
 
 
 def load_queries(queries_path: str) -> list[str]:
@@ -9,19 +10,19 @@ def load_queries(queries_path: str) -> list[str]:
     with open(queries_path, 'r') as f: 
         reader = csv.reader(f)
         queries = [line[0] for line in reader if line != ['Query']]
-
     return queries
 
 def run_scrapper(base: str, limit: int, queries: str, cache_file: str, save_path: str) -> None:
     scrapper = Scrapper(base, limit = limit, cache_file = cache_file)
 
-    for query in queries: 
+    for query in tqdm(queries): 
+        print(query)
         _ = scrapper.query(query, save_path = save_path)
 
 def main():
     base = 'https://google.com/search?'
     queries = load_queries("eval/evaluation_queries.csv")
-    run_scrapper(base, 20, queries[20:25], cache_file = 'files/cache.json', save_path = 'files/processed_search_results/results.csv')
+    run_scrapper(base, 20, queries[193:], cache_file = 'files/cache.json', save_path = 'files/processed_search_results/results.csv')
 
 if __name__ == '__main__':
     main()
