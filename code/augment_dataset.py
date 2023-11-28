612c0e98661ee726501c96a116b93d1eb2ff13d9
haleyej <haleyej@umich.edu>
2023-11-27 21:11:33 -0500

    i am going to create a dataset that is so augmented

--- code/augment_dataset.py
+++ code/augment_dataset.py
@@ -1,5 +1,6 @@
 from doc_augmentor import DocumentAugmentor
 import json 
+import jsonlines
 import os
 from tqdm import tqdm
 
@@ -20,7 +21,7 @@ def read_dataset(path: str, text_key: str) -> dict[int, str]:
 
     return docid_to_text
 
-def augment_dataset(docs: dict[int, str], stopwords:str = 'english') -> dict[int, str]:
+def augment_dataset(docs: dict[int, str], stopwords: str = 'english') -> dict[int, str]:
     docids = list(docs.keys())
     augmentor = DocumentAugmentor(docs, stopwords)
 
@@ -32,21 +33,21 @@ def create_augmented_dataset(augmented_posts: dict[int, str], dataset_path: str,
     comments_data = read_dataset(dataset_path, 'text')
     docids = list(augmented_posts.keys())
 
-    docid_to_aug_full_text = {}
+    docid_to_aug_full_text = []
     for docid in tqdm(docids):
         augmentated_post = augmented_posts[docid]
         comments = comments_data[docid]
 
-        docid_to_aug_full_text[docid] = augmentated_post + " " + comments
+        docid_to_aug_full_text.append({"docid": docid, "text": augmentated_post + " " + comments})
 
-    with open(save_path, 'w') as f: 
-        print(json.dumps(docid_to_aug_full_text), f)
+    with jsonlines.open(save_path, 'w') as f: 
+        f.write_all(docid_to_aug_full_text)
 
 
 def main():
     os.chdir("code")
     path = "../data/ask_reddit_posts_v2.jsonl"
-    docid_to_post = read_dataset(path, text_key = 'text') 
+    docid_to_post = read_dataset(path, text_key = 'title') 
 
     augmented_dataset = augment_dataset(docid_to_post)
     create_augmented_dataset(augmented_dataset, '../data/ask_reddit_posts_v2.jsonl', '../data/ask_reddit_posts_augmented.jsonl')
